c51c0e4fc2d2970b7c840c53b4dc9230bb4a6d15
Arvand Alfarizki Pratama <90150213+ArvandAlfarizki@users.noreply.github.com>
2024-09-17 20:21:00 +0700

    Commit constans
    Commit constans dart

---
+++ lib/constans.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xffffffff);
+Color textColor = Color(000000);
+Color textkaduaColor = Color(0x968B8B);
+Color buttonColor = Color(0x7B6F6);
+Color textpencetColor = Color(0x585555);
+
+TextStyle textStyle = GoogleFonts.balooDa2(
+  color: textColor,
+);
+TextStyle textkaduaStyle = GoogleFonts.balooDa2(
+  color: textkaduaColor,
+);
+TextStyle textpencetStyle = GoogleFonts.balooDa2(
+  color: textpencetColor,
+);
+
+FontWeight bold = FontWeight.bold;
