fbd6069b3441163d9196cb37205a846b99afac2a
ressdn <118348232+ReszaDN@users.noreply.github.com>
2024-09-21 05:24:54 +0800

    profile under development
    profile under development

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,4 +1,6 @@
+import 'package:apkdemo/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -6,12 +8,33 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the Home Screen!'),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Home Screen'),
+      // ),
+      body: Container(
+          margin: EdgeInsets.only(left: 15.0, top: 35.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return const HomeScreen();
+                      }));
+                    },
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 160),
+                child: Center(child: Text('Feature Under Development')),
+              )
+            ],
+          )),
     );
   }
 }
